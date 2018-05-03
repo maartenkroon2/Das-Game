ff62a4445a7e6830c4eaf9a26e50b085f350cfa8
DDanielV <38952804+DDanielV@users.noreply.github.com>
2018-05-03 19:49:10 +0200

    Added commander

--- Assets/Scripts/Commander.cs
+++ Assets/Scripts/Commander.cs
@@ -1,19 +1,71 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using UnityEngine.UI;
+using UnityEngine.UI;
 
 public class Commander : PlayerCharacter
-{ 
+{
+    //private GameObject plane;
+
+    private Terrain terrain;
+    [SerializeField]
+    private Shader shader;
+    private RawImage rawimage;
 
     // Use this for initialization
     protected override void Start()
     {
         base.Start();
+
+        //plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        terrain = Terrain.activeTerrain;
+        rawimage = GetComponentInChildren<RawImage>();
+
+
+        Createheighttexture();
     }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void Createheighttexture()
+    {
+        int terrain_resolution = terrain.terrainData.heightmapResolution - 1;
+        Texture2D texture = new Texture2D(terrain_resolution, terrain_resolution);
+        rawimage.texture = texture;
+        //plane.GetComponent<Renderer>().material.mainTexture = texture;
+        //plane.GetComponent<Renderer>().material.shader = shader;
+        //plane.transform.position = new Vector3(terrain.transform.position.x + terrain.terrainData.size.x / 2, terrain.transform.position.y, terrain.transform.position.z + terrain.terrainData.size.z / 2);
+        //plane.transform.localScale = new Vector3(terrain.terrainData.size.x / 10, 0, terrain.terrainData.size.z / 10);
+
+        //find lowest point in the map
+        float min_height = terrain.terrainData.GetHeight(0, 0);
+        for (int i = 0; i < terrain_resolution; i++)
+        {
+            for (int j = 0; j < terrain_resolution; j++)
+            {
+                min_height = Mathf.Min(min_height, terrain.terrainData.GetHeight(i, j));
+            }
+        }
+
+        //create a color and draw it on the texture
+        float waterlevel = terrain.transform.position.y * -1;
+        for (int i = 0; i < terrain_resolution; i++)
+        {
+            for (int j = 0; j < terrain_resolution; j++)
+            {
+                float terrainheight = terrain.terrainData.GetHeight(i, j);
+                Color color = new Color(0, 0, 0, 0);
+                if (terrainheight < waterlevel)
+                {
+                    color = new Color(0, Mathf.Floor((terrainheight - min_height) / (waterlevel - min_height) * 10) / 10, 1, 1); //color between blue(0,0,1,1) and cyan(0,1,1,1)
+                }
+                else { color = new Color(0.95f, 0.9f, 0.7f, 1); /*pale yellow*/ }
+                texture.SetPixel(terrain_resolution - i, terrain_resolution - j, color); //rotates the location 180° before saving the pixel, don't know why this is necessary but it works
+            }
+        }
+        texture.Apply();
+    }
 }
--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -7,10 +7,11 @@ public class Driver : PlayerCharacter
     private GameObject submarine;
     private Rigidbody rigidbody;
 
-    private float steering, throttle, depth_throttle, max_stearing, input_steering, speed;
+    private float steering, throttle, max_stearing, input_steering, speed; //depth_throttle
     private float[] stearing_arr = new float[10];
     private int stearing_arr_index;
     private Slider Slider_Throttle;
+    private Text speedtext;
 
     // Use this for initialization.
     protected override void Start()
@@ -20,6 +21,7 @@ public class Driver : PlayerCharacter
         rigidbody = submarine.GetComponent<Rigidbody>();
         Slider_Throttle = transform.GetComponentInChildren<Slider>();
         //Slider_Throttle = GameObject.FindGameObjectWithTag("Slider_Throttle").GetComponent<Slider>();
+        speedtext = transform.GetComponentInChildren<Text >();
 
 
         base.Start();
@@ -44,19 +46,20 @@ public class Driver : PlayerCharacter
         input_steering = total / 10;
 
         throttle = Slider_Throttle.value / Slider_Throttle.maxValue;
-        max_stearing = 1f - rigidbody.velocity.magnitude / 12;
+        max_stearing = 1f;// - rigidbody.velocity.magnitude / 120;
         steering = Mathf.Min(Mathf.Max(input_steering * 2, -max_stearing), max_stearing) + Input.GetAxis("Horizontal");
         //depth_throttle = Slider_depth.value;
         speed = rigidbody.velocity.magnitude * Vector3.Dot(rigidbody.transform.forward, Vector3.Normalize(rigidbody.velocity));
+        speedtext.text = "Speed: " + speed.ToString("F1");
     }
 
     // FixedUpdate is called every fixed timestep.
     // Super floaty experimental movement, just for testing.
     private void FixedUpdate()
     {
-        rigidbody.AddTorque(rigidbody.transform.up * speed * steering);
+        rigidbody.AddTorque(rigidbody.transform.up * steering * speed * 50000);
         rigidbody.velocity = rigidbody.transform.forward * speed;
-        rigidbody.AddForce(rigidbody.transform.forward * 10000000 * throttle);
-        rigidbody.MovePosition(rigidbody.transform.position + rigidbody.transform.up * depth_throttle *10000000 * Time.deltaTime);
+        rigidbody.AddForce(rigidbody.transform.forward * 300000 * throttle);
+        //rigidbody.MovePosition(rigidbody.transform.position + rigidbody.transform.up * depth_throttle * Time.deltaTime);
     }
 }
