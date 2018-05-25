709474d0e65f55b447452f4cddd4eb707f9dee1f
DDanielV <38952804+DDanielV@users.noreply.github.com>
2018-05-25 22:35:17 +0200

    Revert "Merge branch 'driver'"
    This reverts commit 10014506d1d8f15009044afda8827845f58e1fe6, reversing
    changes made to 251daad31f7e8fe7c99af283fcc9ed25c4a89f21.

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -7,16 +7,11 @@ public class Driver : PlayerCharacter
     private GameObject submarine;
     private Rigidbody rigidbody;
 
-    private float steering, throttle, max_stearing, input_steering, speed, depth_throttle, max_speed;
+    private float steering, throttle, max_stearing, input_steering, speed; //depth_throttle
     private float[] stearing_arr = new float[10];
     private int stearing_arr_index;
-
-    //canvas
-    private Slider slider_throttle, slider_depth;
-    private Image spr_steeringwheel_left, spr_steeringwheel_right, spr_steeringwheel_pointer;
-    private Text text_speed, text_depth, text_direction;
-    private Vector3 center_steeringwheel;
-
+    private Slider Slider_Throttle;
+    private Text speedtext;
 
     // Use this for initialization.
     protected override void Start()
@@ -24,12 +19,10 @@ public class Driver : PlayerCharacter
         // Get a reference to the submarine and it's rigidbody
         submarine = GameObject.FindGameObjectWithTag("Submarine");
         rigidbody = submarine.GetComponent<Rigidbody>();
+        Slider_Throttle = transform.GetComponentInChildren<Slider>();
+        //Slider_Throttle = GameObject.FindGameObjectWithTag("Slider_Throttle").GetComponent<Slider>();
+        speedtext = transform.GetComponentInChildren<Text >();
 
-        Get_sliders();
-        Get_images();
-        Get_texts();
-        center_steeringwheel = spr_steeringwheel_pointer.transform.position;
-        max_speed = (100000 / rigidbody.drag - 0.01f * 100000) / rigidbody.mass; //max_speed = (addforce / drag - Time.fixedDeltaTime * addforce) / mass
 
         base.Start();
         
@@ -38,113 +31,13 @@ public class Driver : PlayerCharacter
         rigidbody.inertiaTensorRotation = Quaternion.identity;
     }
 
-    private void Get_sliders()
-    {
-        Slider[] array_sliders = transform.GetComponentsInChildren<Slider>();
-        foreach (Slider slider in array_sliders)
-        {
-            switch (slider.gameObject.name)
-            {
-                case "Slider_Throttle":
-                    slider_throttle = slider;
-                    break;
-
-                case "Slider_Depth":
-                    slider_depth = slider;
-                    break;
-
-                default:
-                    Debug.Log("Unkown slider component: " + slider.gameObject.name);
-                    break;
-            }
-        }
-    }
-    private void Get_images()
-    {
-        Image[] array_images = transform.GetComponentsInChildren<Image>();
-        foreach (Image image in array_images)
-        {
-            switch (image.gameObject.name)
-            {
-                case "Steeringwheel_left":
-                    spr_steeringwheel_left = image;
-                    break;
-
-                case "Steeringwheel_right":
-                    spr_steeringwheel_right = image;
-                    break;
-
-                case "Steeringwheel_pointer":
-                    spr_steeringwheel_pointer = image;
-                    break;
-
-                case "Handle":
-                    //do nothing, is an image of the slider
-                    break;
-
-                case "Background":
-                    //do nothing, is an image of the slider
-                    break;
-
-                default:
-                    Debug.Log("Unkown image component: " + image.gameObject.name);
-                    break;
-            }
-        }
-    }
-    private void Get_texts()
-    {
-        Text[] array_texts = transform.GetComponentsInChildren<Text>();
-        foreach (Text text in array_texts)
-        {
-            switch (text.gameObject.name)
-            {
-                case "Text_Speed":
-                    text_speed = text;
-                    break;
-
-                case "Text_Depth":
-                    text_depth = text;
-                    break;
-
-                case "Text_Direction":
-                    text_direction = text;
-                    break;
-
-                default:
-                    Debug.Log("Unkown text component: " + text.gameObject.name);
-                    break;
-            }
-        }
-    }
-
     // Update is called once per frame.
     // Get the player input for controlling the submarine
     private void Update()
     {
-        Get_inputs();
-        Update_canvas();
-    }
+        //horizontal = Input.GetAxis("Horizontal");
+        //vertical = Input.GetAxis("Vertical");
 
-    // FixedUpdate is called every fixed timestep.
-    private void FixedUpdate()
-    {
-        rigidbody.AddTorque(rigidbody.transform.up * steering * speed * 100000);
-        rigidbody.velocity = rigidbody.transform.forward * speed;
-        rigidbody.AddForce(rigidbody.transform.forward * 100000 * throttle);
-
-        if (rigidbody.transform.position.y + depth_throttle * Time.deltaTime > 0)
-        {
-            rigidbody.MovePosition(rigidbody.transform.position + new Vector3(0, -rigidbody.transform.position.y, 0));
-        }
-        else
-        {
-            rigidbody.MovePosition(rigidbody.transform.position + rigidbody.transform.up * depth_throttle * Time.deltaTime);
-        }
-    }
-
-    private void Get_inputs()
-    {
         stearing_arr_index++;
         if (stearing_arr_index >= 10) { stearing_arr_index = 0; }
         stearing_arr[stearing_arr_index] = Input.acceleration.x;
@@ -152,25 +45,28 @@ public class Driver : PlayerCharacter
         for (int i = 0; i < 10; i++) { total += stearing_arr[i]; }
         input_steering = total / 10;
 
-        throttle = slider_throttle.value / slider_throttle.maxValue;
-        max_stearing = 1f - rigidbody.velocity.magnitude / max_speed * 0.7f; //0.7f betekent dat bij max speed nog (1-0.7)30% stuurkracht over hebt
+        throttle = Slider_Throttle.value / Slider_Throttle.maxValue;
+        max_stearing = 1f;// - rigidbody.velocity.magnitude / 120;
         steering = input_steering * 2 + Input.GetAxis("Horizontal");
-        steering = Mathf.Clamp(steering, -max_stearing, max_stearing);
-        depth_throttle = slider_depth.value*10;
+        steering = Mathf.Clamp(steering, -1 *max_stearing, max_stearing);
+        //depth_throttle = Slider_depth.value;
         speed = rigidbody.velocity.magnitude * Vector3.Dot(rigidbody.transform.forward, Vector3.Normalize(rigidbody.velocity));
+        speedtext.text = "Speed: " + speed.ToString("F1");
     }
 
-    private void Update_canvas()
+    // FixedUpdate is called every fixed timestep.
+    // Super floaty experimental movement, just for testing.
+    private void FixedUpdate()
     {
-        spr_steeringwheel_left.fillAmount = max_stearing;
-        spr_steeringwheel_right.fillAmount = max_stearing;
+        //rigidbody.AddTorque(rigidbody.transform.up * steering * speed * 50000);
+        rigidbody.transform.Rotate(rigidbody.transform.up * steering * speed * Time.deltaTime * 0.2f);
+        rigidbody.velocity = rigidbody.transform.forward * speed;
+        rigidbody.AddForce(rigidbody.transform.forward * 100000 * throttle);
 
-        Quaternion steeringwheel_rotation = Quaternion.Euler(0f, 0f, -90 * steering);
-        spr_steeringwheel_pointer.transform.SetPositionAndRotation(center_steeringwheel + steeringwheel_rotation * new Vector3(0, Screen.height/2, 0), steeringwheel_rotation);
-
-        text_direction.text = Quaternion.LookRotation(transform.forward).eulerAngles.y.ToString("F0") + "°";
-        text_speed.text = "Speed: " + speed.ToString("F1");
-        text_depth.text = "Depth: " + transform.position.y.ToString("F1");
+        if (Input.GetKey(KeyCode.LeftShift) && rigidbody.transform.position.y < 0) { rigidbody.MovePosition(rigidbody.transform.position + rigidbody.transform.up * 10 * Time.deltaTime); }
+        if (Input.GetKey(KeyCode.LeftControl)) { rigidbody.MovePosition(rigidbody.transform.position + rigidbody.transform.up * -10 * Time.deltaTime); }
+        if (rigidbody.transform.position.y >= 0) { rigidbody.transform.position = new Vector3(rigidbody.transform.position.x, 0, rigidbody.transform.position.z); }
+        //rigidbody.MovePosition(rigidbody.transform.position + rigidbody.transform.up * depth_throttle * Time.deltaTime);
     }
 
     private void LateUpdate()
