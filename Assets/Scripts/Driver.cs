75c52fced9d167adc6799c7a6bd2cf486a0d374a
DDanielV <38952804+DDanielV@users.noreply.github.com>
2018-05-25 22:31:54 +0200

    Revert "Handmatig terug naar merge test"
    This reverts commit 39033e1b5bc6964d8a8d8657acb3c41a93004241.

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -31,12 +31,101 @@ public class Driver : PlayerCharacter
         rigidbody.inertiaTensorRotation = Quaternion.identity;
     }
 
+<<<<<<< HEAD
+=======
+    private void Get_sliders()
+    {
+        Slider[] array_sliders = transform.GetComponentsInChildren<Slider>();
+        foreach (Slider slider in array_sliders)
+        {
+            switch (slider.gameObject.name)
+            {
+                case "Slider_Throttle":
+                    slider_throttle = slider;
+                    break;
+
+                case "Slider_Depth":
+                    slider_depth = slider;
+                    break;
+
+                default:
+                    Debug.Log("Unkown slider component: " + slider.gameObject.name);
+                    break;
+            }
+        }
+    }
+    private void Get_images()
+    {
+        Image[] array_images = transform.GetComponentsInChildren<Image>();
+        foreach (Image image in array_images)
+        {
+            switch (image.gameObject.name)
+            {
+                case "Steeringwheel_left":
+                    spr_steeringwheel_left = image;
+                    break;
+
+                case "Steeringwheel_right":
+                    spr_steeringwheel_right = image;
+                    break;
+
+                case "Steeringwheel_pointer":
+                    spr_steeringwheel_pointer = image;
+                    break;
+
+                default:
+                    Debug.Log("Unkown image component: " + image.gameObject.name);
+                    break;
+            }
+        }
+    }
+    private void Get_texts()
+    {
+        Text[] array_texts = transform.GetComponentsInChildren<Text>();
+        foreach (Text text in array_texts)
+        {
+            switch (text.gameObject.name)
+            {
+                case "Text_Speed":
+                    text_speed = text;
+                    break;
+
+                case "Text_Depth":
+                    text_depth = text;
+                    break;
+
+                case "Text_Direction":
+                    text_direction = text;
+                    break;
+
+                default:
+                    Debug.Log("Unkown text component: " + text.gameObject.name);
+                    break;
+            }
+        }
+    }
+
+>>>>>>> parent of 59302e8... Driver commit
     // Update is called once per frame.
     // Get the player input for controlling the submarine
     private void Update()
     {
+<<<<<<< HEAD
         //horizontal = Input.GetAxis("Horizontal");
         //vertical = Input.GetAxis("Vertical");
+=======
+        Get_inputs();
+        Update_canvas();
+    }
+
+    // FixedUpdate is called every fixed timestep.
+    private void FixedUpdate()
+    {
+        //rigidbody.AddTorque(rigidbody.transform.up * steering * speed * 50000);
+        rigidbody.transform.Rotate(rigidbody.transform.up * steering * speed * Time.deltaTime * 0.2f);
+        rigidbody.velocity = rigidbody.transform.forward * speed;
+        rigidbody.AddForce(rigidbody.transform.forward * 100000 * throttle);
+>>>>>>> parent of 59302e8... Driver commit
 
         stearing_arr_index++;
         if (stearing_arr_index >= 10) { stearing_arr_index = 0; }
@@ -45,8 +134,13 @@ public class Driver : PlayerCharacter
         for (int i = 0; i < 10; i++) { total += stearing_arr[i]; }
         input_steering = total / 10;
 
+<<<<<<< HEAD
         throttle = Slider_Throttle.value / Slider_Throttle.maxValue;
         max_stearing = 1f;// - rigidbody.velocity.magnitude / 120;
+=======
+        throttle = slider_throttle.value / slider_throttle.maxValue;
+        max_stearing = 1f - rigidbody.velocity.magnitude / max_speed / 1.3f; //1.3f betekent dat bij max speed nog (1/3)=33% stuurkracht over hebt
+>>>>>>> parent of 59302e8... Driver commit
         steering = input_steering * 2 + Input.GetAxis("Horizontal");
         steering = Mathf.Clamp(steering, -1 *max_stearing, max_stearing);
         //depth_throttle = Slider_depth.value;
@@ -58,10 +152,18 @@ public class Driver : PlayerCharacter
     // Super floaty experimental movement, just for testing.
     private void FixedUpdate()
     {
+<<<<<<< HEAD
         //rigidbody.AddTorque(rigidbody.transform.up * steering * speed * 50000);
         rigidbody.transform.Rotate(rigidbody.transform.up * steering * speed * Time.deltaTime * 0.2f);
         rigidbody.velocity = rigidbody.transform.forward * speed;
         rigidbody.AddForce(rigidbody.transform.forward * 100000 * throttle);
+=======
+        spr_steeringwheel_left.fillAmount = max_stearing;
+        spr_steeringwheel_right.fillAmount = max_stearing;
+
+        Quaternion steeringwheel_rotation = Quaternion.Euler(0f, 0f, -90 * steering);
+        spr_steeringwheel_pointer.transform.SetPositionAndRotation(center_steeringwheel + steeringwheel_rotation * new Vector3(0, 320, 0), steeringwheel_rotation);
+>>>>>>> parent of 59302e8... Driver commit
 
         if (Input.GetKey(KeyCode.LeftShift) && rigidbody.transform.position.y < 0) { rigidbody.MovePosition(rigidbody.transform.position + rigidbody.transform.up * 10 * Time.deltaTime); }
         if (Input.GetKey(KeyCode.LeftControl)) { rigidbody.MovePosition(rigidbody.transform.position + rigidbody.transform.up * -10 * Time.deltaTime); }
