18d3db742fc649d4db4119904e145170fcdd7027
maartenkroon2 <maartenkroon2@gmail.com>
2018-07-25 20:44:10 +0200

    Added some alternative to object pooling and destroy torpedo's

--- Assets/Scripts/Torpedo.cs
+++ Assets/Scripts/Torpedo.cs
@@ -1,16 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Torpedo : MonoBehaviour {
     [SerializeField]
-    private float speed;
-
+    private float startSpeed, maxSpeed, acceleration;
     private Rigidbody rigidbody;
 
     private void Start()
     {
+        // Gives the torpedo an initial speed, maybe this should add the current speed of the submarine.
         rigidbody = GetComponent<Rigidbody>();
-        rigidbody.velocity = transform.forward * speed;     
-    }    
+        rigidbody.velocity = transform.forward * startSpeed;
+
+        // Destroy the torpedo after 10 seconds, this is enough for them to escape view. 
+        // Object pooling could be used here but is not really needed.
+        Destroy(gameObject, 10);
+    }
+
+    private void FixedUpdate()
+    {
+        // Increases the speed of the torpedo over time until the max speed is reached.
+        rigidbody.AddForce(transform.forward * acceleration);
+        rigidbody.velocity = rigidbody.velocity.magnitude > maxSpeed ? transform.forward * maxSpeed : rigidbody.velocity;   
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("boom" + collision.gameObject.name);
+    }
 }
