05f009d91b53dea5480c011dc2800fed952f57e4
DDanielV <38952804+DDanielV@users.noreply.github.com>
2018-11-04 21:38:19 +0100

    updated Unity naar versie  2017.4.10f1 en createn van android apk werkt weer

--- Assets/Scripts/CharacterMenu.cs
+++ Assets/Scripts/CharacterMenu.cs
@@ -4,9 +4,15 @@
 public class CharacterMenu : MonoBehaviour {
     private PlayerCharacter character;
 
+    public void Start()
+    {
+        //bug in unity, als fog niet aan staat bij createn van de APK wordt de shader hiervoor ook niet toegevoegd aan de apk, daarom wordt hier de fog uigezet aan het begin van het spel
+        RenderSettings.fog = false;
+    }
+
     // Destroys the current PlayerClass if there is any and creates a new one.
     // This method is called by the buttons on the CharacterMenu panel.    
-	public void SetPlayerCharacter(PlayerCharacter character)
+    public void SetPlayerCharacter(PlayerCharacter character)
     {
         if (this.character != null)
         {
